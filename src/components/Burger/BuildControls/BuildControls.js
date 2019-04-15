fd28b654eb097ce77b001a7b595908be647ff0f3
Kovács Áron László <kovacs.aron@arteries.hu>
2019-04-15 18:54:02 +0200

    form validation; checkout form

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -26,7 +26,7 @@ const buildControls = (props) => (
             disabled={!props.purchasable} 
             onClick={props.ordered}>
                 ORDER NOW
-            </button>
+        </button>
     </div>
 );
 
